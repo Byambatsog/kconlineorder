b260c97619fb30f2d58105c99668dfd32bf36cba
byambatsog <bchimed@luc.edu>
2015-11-25 14:46:54 -0600

    initial DB project

--- model/menu_category_db.php
+++ model/menu_category_db.php
@@ -1,22 +1,4 @@
 <?php
-function get_products_by_category($category_id) {
-    global $db;
-    $query = 'SELECT * FROM products
-              WHERE categoryID = :category_id
-              ORDER BY productID';
-    try {
-        $statement = $db->prepare($query);
-        $statement->bindValue(':category_id', $category_id);
-        $statement->execute();
-        $result = $statement->fetchAll();
-        $statement->closeCursor();
-        return $result;
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        display_db_error($error_message);
-    }
-}
-
 function get_categories() {
     global $db;
     $query = 'SELECT categoryID, name, picture, ranking, status FROM menucategories';
