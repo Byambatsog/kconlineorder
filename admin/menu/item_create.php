a6da6e632dac1775c334e6874faf35aa781b0d2d
byambatsog <bchimed@luc.edu>
2015-11-27 13:39:00 -0600

    Form validation

--- admin/menu/item_create.php
+++ admin/menu/item_create.php
@@ -7,6 +7,14 @@
     $success_notification = '';
     $error_notification = '';
 
+    $error_name = '';
+    $error_category = '';
+    $error_unitPrice = '';
+    $error_calories = '';
+    $error_ranking = '';
+
+    $formValidation = true;
+
     $categories = get_categories();
     $itemID = '';
 
@@ -15,6 +23,7 @@
         $itemID = $_POST['itemID'];
 
         try {
+
             $item = array();
             $item['itemID'] = $_POST['itemID'];
             $item['categoryID'] = $_POST['categoryID'];
@@ -28,35 +37,53 @@
             else $item['status'] = 'D';
             $item['ranking'] = $_POST['ranking'];
 
+            if(empty($item['name'])) {
+                $error_name = 'Name must be filled out';
+                $formValidation = false;
+            }
 
-            if(empty($item['itemID'])){
-                session_start();
-                $item['createdBy'] = $_SESSION['userId'];
-                add_item($item);
-            } else {
-                update_item($item);
+            if(empty($item['categoryID'])) {
+                $error_category = 'Category must be selected';
+                $formValidation = false;
+            }
+
+            if(empty($item['unitPrice'])) {
+                $error_unitPrice = 'Unit price must be filled out';
+                $formValidation = false;
+            } else if(!is_numeric($item['unitPrice'])) {
+                $error_unitPrice = 'Unit price must be valid number';
+                $formValidation = false;
+            }
+
+            if(empty($item['calories'])) {
+                $error_calories = 'Calories must be filled out';
+                $formValidation = false;
+            } else if(!is_numeric($item['calories'])) {
+                $error_calories = 'Calories must be valid number';
+                $formValidation = false;
+            }
+
+            if(empty($item['ranking'])) {
+                $error_ranking = 'Ranking must be filled out';
+                $formValidation = false;
+            } else if(!is_numeric($item['ranking'])) {
+                $error_ranking = 'Ranking must be valid number';
+                $formValidation = false;
+            }
+
+            if($formValidation){
+                if(empty($item['itemID'])){
+                    session_start();
+                    $item['createdBy'] = $_SESSION['userId'];
+                    add_item($item);
+                } else {
+                    update_item($item);
+                }
+                $success_notification = 'Successfully saved';
             }
         } catch (Exception $e) {
             $error_notification = $e->getMessage();
         }
-
-        $success_notification = 'Successfully saved';
-
-
-
-//        // Validate inputs
-//        if (empty($code) || empty($name) || empty($description) ||
-//            empty($price) ) {
-//            $error = 'Invalid product data.
-//                      Check all fields and try again.';
-//            include('../../errors/error.php');
-//        } else {
-//            $categories = get_categories();
-//            $product_id = add_product($category_id, $code, $name,
-//                $description, $price, $discount_percent);
-//            $product = get_product($product_id);
-//            include('product_view.php');
-//        }
     } else {
         if(isset($_GET['itemID'])) $itemID = $_GET['itemID'];
         if(!empty($itemID)) $item = get_item($itemID);
@@ -99,29 +126,26 @@
 
         <div class="row">
             <div class="col-xs-6">
-<!--                <div class="form-group has-error">-->
-                <div class="form-group">
-                    <label class="control-label strong" for="name">Name</label>
-                    <input id="name" name="name" class="form-control input-sm" value="<?php if(!empty($itemID)) echo $item['name'];?>"/>
-    <!--                <c:if test="${!empty titleError}">-->
-    <!--                    <span class="help-block">${titleError}</span>-->
-    <!--                </c:if>-->
+                <div class="form-group <?php if(!empty($error_name)) echo 'has-error';?>">
+                    <label class="control-label strong" for="name">Name <span class="f_req">*</span></label>
+                    <input id="name" name="name" class="form-control input-sm" value="<?php if(!empty($item)) echo $item['name'];?>"/>
+                    <?php if(!empty($error_name)) { ?>
+                        <span class="help-block"><?php echo $error_name;?></span>
+                    <?php }?>
                 </div>
             </div>
             <div class="col-xs-6">
-    <!--            <c:set var="titleAliasError"><form:errors path="titleAlias"/></c:set>-->
-    <!--            <div class="form-group <c:if test="${!empty titleAliasError}">has-error</c:if>">-->
-                <div class="form-group">
-                    <label class="control-label strong" for="category">Category</label>
+                <div class="form-group <?php if(!empty($error_category)) echo 'has-error';?>">
+                    <label class="control-label strong" for="category">Category <span class="f_req">*</span></label>
                     <select id="categoryID" name="categoryID" class="form-control input-sm">
                         <option value="">...</option>
                         <?php foreach ($categories as $category) : ?>
-                            <option value="<?php echo $category['categoryID']; ?>" <?php if(!empty($itemID)&&$category['categoryID']==$item['categoryID']) echo 'selected="selected"';?>><?php echo $category['name']; ?></option>
+                            <option value="<?php echo $category['categoryID']; ?>" <?php if(!empty($item)&&$category['categoryID']==$item['categoryID']) echo 'selected="selected"';?>><?php echo $category['name']; ?></option>
                         <?php endforeach; ?>
                     </select>
-        <!--            <c:if test="${!empty titleAliasError}">-->
-        <!--                <span class="help-block">${titleAliasError}</span>-->
-        <!--            </c:if>-->
+                    <?php if(!empty($error_category)) { ?>
+                        <span class="help-block"><?php echo $error_category;?></span>
+                    <?php }?>
                 </div>
             </div>
         </div>
@@ -129,44 +153,59 @@
             <div class="col-xs-12">
                 <div class="form-group">
                     <label class="control-label strong" for="picture">Picture (URL)</label>
-                    <input id="picture" name="picture" class="form-control input-sm" value="<?php if(!empty($itemID)) echo $item['picture'];?>"/>
+                    <input id="picture" name="picture" class="form-control input-sm" value="<?php if(!empty($item)) echo $item['picture'];?>"/>
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-xs-3">
-                <div class="form-group">
-                    <label class="control-label strong" for="unitPrice">Unit Price ($)</label>
-                    <input id="unitPrice" name="unitPrice" class="form-control input-sm" value="<?php if(!empty($itemID)) echo $item['unitPrice'];?>"/>
+                <div class="form-group <?php if(!empty($error_unitPrice)) echo 'has-error';?>">
+                    <label class="control-label strong" for="unitPrice">Unit Price ($) <span class="f_req">*</span></label>
+                    <input readonly="readonly" id="unitPrice" name="unitPrice" class="form-control input-sm" value="<?php if(!empty($item)) echo $item['unitPrice']; else echo '0';?>"/>
+                    <div class="sepH_b" style="margin-top: 5px;">
+                        <div class="unitprice_slider"></div>
+                    </div>
+                    <?php if(!empty($error_unitPrice)) { ?>
+                        <span class="help-block"><?php echo $error_unitPrice;?></span>
+                    <?php }?>
                 </div>
             </div>
             <div class="col-xs-3">
-                <div class="form-group">
-                    <label class="control-label strong" for="calories">Calories (K)</label>
-                    <input id="calories" name="calories" class="form-control input-sm" value="<?php if(!empty($itemID)) echo $item['calories'];?>"/>
+                <div class="form-group <?php if(!empty($error_calories)) echo 'has-error';?>">
+                    <label class="control-label strong" for="calories">Calories (K) <span class="f_req">*</span></label>
+                    <input id="calories" name="calories" class="form-control input-sm" value="<?php if(!empty($item)) echo $item['calories'];  else echo '0';?>"/>
+                    <div class="sepH_b" style="margin-top: 5px;">
+                        <div class="calories_slider"></div>
+                    </div>
+                    <?php if(!empty($error_calories)) { ?>
+                        <span class="help-block"><?php echo $error_calories;?></span>
+                    <?php }?>
                 </div>
             </div>
             <div class="col-xs-3">
-                <div class="form-group">
-                    <label class="control-label strong" for="ranking">Ranking</label>
-                    <input id="ranking" name="ranking" class="form-control input-sm" value="<?php if(!empty($itemID)) echo $item['ranking'];?>"/>
+                <div class="form-group <?php if(!empty($error_ranking)) echo 'has-error';?>">
+                    <label class="control-label strong" for="ranking">Ranking <span class="f_req">*</span></label>
+                    <input id="ranking" name="ranking" class="form-control input-sm" value="<?php if(!empty($item)) echo $item['ranking'];  else echo '1';?>"/>
+                    <div class="sepH_b" style="margin-top: 5px;">
+                        <div class="ranking_slider"></div>
+                    </div>
+                    <?php if(!empty($error_ranking)) { ?>
+                        <span class="help-block"><?php echo $error_ranking;?></span>
+                    <?php }?>
                 </div>
             </div>
             <div class="col-xs-3">
                 <div class="form-group">
                     <label class="control-label strong">Enabled</label>
                     <div class="status-switch" id="status-switch" data-on="info">
-                        <input type="checkbox" name="status" id="status" <?php if(empty($itemID)||$item['status']=='E') echo 'checked="checked';?>"/>
+                        <input type="checkbox" name="status" id="status" <?php if(empty($item)||$item['status']=='E') echo 'checked="checked';?>"/>
                     </div>
                 </div>
             </div>
         </div>
         <div class="form-group">
             <label class="control-label strong" for="description">Description</label>
-            <textarea name="description" id="description" rows="4" class="form-control"><?php if(!empty($itemID)) echo $item['description'];?></textarea>
-<!--        <c:if test="${!empty noteError}">-->
-<!--            <span class="help-block">${noteError}</span>-->
-<!--        </c:if>-->
+            <textarea name="description" id="description" rows="4" class="form-control"><?php if(!empty($item)) echo $item['description'];?></textarea>
         </div>
 
         <div class="formSep"></div>
@@ -212,5 +251,35 @@
         $('#status-switch').bootstrapSwitch('setSizeClass', 'switch-small');
         $('#status-switch').bootstrapSwitch('setOnLabel', 'Yes');
         $('#status-switch').bootstrapSwitch('setOffLabel', 'No');
+
+        $( ".unitprice_slider" ).slider({
+            value:<?php if(!empty($item)) echo $item['unitPrice']; else echo '0';?>,
+            min: 0,
+            max: 50,
+            step: 0.05,
+            slide: function( event, ui ) {
+                $( "#unitPrice" ).val(ui.value );
+            }
+        });
+
+        $( ".calories_slider" ).slider({
+            value:<?php if(!empty($item)) echo $item['calories']; else echo '0';?>,
+            min: 0,
+            max: 2000,
+            step: 10,
+            slide: function( event, ui ) {
+                $( "#calories" ).val(ui.value );
+            }
+        });
+
+        $( ".ranking_slider" ).slider({
+            value:<?php if(!empty($item)) echo $item['ranking']; else echo '1';?>,
+            min: 1,
+            max: 10,
+            step: 1,
+            slide: function( event, ui ) {
+                $( "#ranking" ).val(ui.value );
+            }
+        });
     });
 </script>
